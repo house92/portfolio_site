30f43696868c783cafe4dfa36a4adc77cf7f110d
Sam <samuel.house@ntlworld.com>
2017-01-28 02:23:36 +0000

    optimised for most mobile; found gap in tablet

--- app/assets/javascripts/components/homePage.jsx
+++ app/assets/javascripts/components/homePage.jsx
@@ -3,23 +3,88 @@ import { Row, Col, Image, Nav, NavItem, OverlayTrigger, Tooltip } from 'react-bo
 import Header from './header.jsx';
 import Skills from './skills.jsx';
 
+const orientation = {landscape: 'background-2.png', portrait: 'background-portrait.png'};
+
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { background: orientation.landscape };
+  }
+
+  componentWillMount() {
+    if (window.innerWidth < window.innerHeight && window.innerWidth < 500) {
+      this.setState({ background: orientation.portrait });
+    } else {
+      this.setState({ background: orientation.landscape });
+    }
+  }
+
+  componentDidMount() {
+    var screenWidth;
+    setTimeout(() => {
+      screenWidth = window.innerWidth;
+    }, 10);
+    window.addEventListener('resize', () => {
+      if (window.innerWidth != screenWidth) {
+        this.forceUpdate();
+      }
+      if (window.innerWidth < window.innerHeight && window.innerWidth < 500) {
+        this.setState({ background: orientation.portrait });
+      } else {
+        this.setState({ background: orientation.landscape });
+      }
+    });
+  }
+
   render() {
     var languages = [{name: "HTML5", image: "HTML5-logo.png"}, {name: "Javascript", image: "javascript.png"}, {name: "ReactJS", image: "dark-reactJS.svg"}, {name: "jQuery", image: "jQuery.png"}, {name: "Ruby", image: "ruby-logo.png"}, {name: "Ruby on Rails", image: "Ruby_on_Rails-logo.png"}, {name: "PostgreSQL", image: "postgresql-logo.png"}];
+    var firstLanguage;
     var languagesDiv = languages.map((language, i) => {
       var tooltip = <Tooltip id={`tooltip${i + 1}`}>{language.name}</Tooltip>;
-      return (
-        <li key={`language${i + 1}`}>
-          <OverlayTrigger placement="top" overlay={tooltip}>
-            <Image src={`/assets/${language.image}`} alt={language.name} responsive />
-          </OverlayTrigger>
-        </li>
-      );
+      if (window.innerWidth < 440 ) {
+        if (i % 2 == 0 && i != languages.length -1) {
+          firstLanguage = language;
+          firstLanguage.tooltip = tooltip;
+        } else if (i == languages.length -1) {
+          return (
+            <Row key={`language${i + 1}`} style={{ marginBottom: '3rem' }}>
+              <Col xs={6} xsOffset={3}>
+                <OverlayTrigger placement="top" overlay={tooltip}>
+                  <Image src={`/assets/${language.image}`} alt={language.name} responsive />
+                </OverlayTrigger>
+              </Col>
+            </Row>
+          );
+        } else {
+          return (
+            <Row key={`language${i + 1}`} style={{ marginBottom: '3rem' }}>
+              <Col xs={6}>
+                <OverlayTrigger placement="top" overlay={firstLanguage.tooltip}>
+                  <Image src={`/assets/${firstLanguage.image}`} alt={firstLanguage.name} responsive />
+                </OverlayTrigger>
+              </Col>
+              <Col xs={6}>
+                <OverlayTrigger placement="top" overlay={tooltip}>
+                  <Image src={`/assets/${language.image}`} alt={language.name} responsive />
+                </OverlayTrigger>
+              </Col>
+            </Row>
+          );
+        }
+      } else {
+        return (
+          <li key={`language${i + 1}`}>
+            <OverlayTrigger placement="top" overlay={tooltip}>
+              <Image src={`/assets/${language.image}`} alt={language.name} responsive />
+            </OverlayTrigger>
+          </li>
+        );
+      }
     });
 
     return (
       <Header className="home">
-        <Image src="/assets/background-2.png" alt="Sam House - web developer" className="main-img" />
+        <Image src={`/assets/${this.state.background}`} alt="Sam House - web developer" className="main-img" />
           <h1 className="hidden">Sam House - web developer</h1>
 
           <div className="about">
--- app/assets/javascripts/components/skills.jsx
+++ app/assets/javascripts/components/skills.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col, Image } from 'react-bootstrap';
 
-var design = {name: 'Design', image: "pencil.png", content: (
+const design = {name: 'Design', image: "pencil.png", content: (
   <div className="info text-center">
     <p>
       {`I offer creative and relevant design, ensuring the highest level of presentation for your project. The most important thing is that content is simple and direct. You want to come across loud and clear to your end-users.`}
@@ -9,7 +9,7 @@ var design = {name: 'Design', image: "pencil.png", content: (
   </div>
 )};
 
-var development = {name: 'Development', image: "smartphone.png", content: (
+const development = {name: 'Development', image: "smartphone.png", content: (
   <div className="info text-center">
     <p>
       {`Staying up-to-date in the ever changing scene of web development is a challenge - that's where I come in. I keep on top of the latest tools and tricks to optimise your end-users' experience. At the same time, you don't want them to drown in gimicks! I'll help you to keep out the clutter.`}
@@ -17,7 +17,7 @@ var development = {name: 'Development', image: "smartphone.png", content: (
   </div>
 )};
 
-var audience = {name: 'Audience', image: "silhouette.png", content: (
+const audience = {name: 'Audience', image: "silhouette.png", content: (
   <div className="info text-center">
     <p>
       {`Audience matters. I'm committed to keeping that in mind at every step throughout the design and development processes. Setting up a platform for your music? Sharp, sleek and edgy is the way to go. Floral gift cards? ...Not so much.`}
@@ -101,7 +101,7 @@ export default class Skills extends Component {
     }
   }
 
-  render() {
+  componentDidMount() {
     window.addEventListener('resize', () => {
       var infoName = document.getElementById('insert-title');
       if (window.innerWidth < 992) {
@@ -111,56 +111,118 @@ export default class Skills extends Component {
         infoName.textContent = null;
       }
     });
-    var skills = [design, development, audience];
-    var skillsDiv = skills.map((skill, i) => {
-      return (
-        <li key={`skill${i + 1}`}>
-          <div className="title-container">
-            <h3 id={`${skill.name.toLowerCase()}Title`} className={`title`}>{skill.name}</h3>
-          </div>
-          <div className="icon-container">
-            <Image src={`/assets/${skill.image}`} circle className={`rotate${skill.name == this.state.info.name ? ' selected' : ''}`} data-name={skill.name} onClick={this.handleClick} onMouseOver={this.rotate} onMouseOut={this.unrotate} />
-          </div>
-        </li>
+  }
+
+  render() {
+    const skills = [design, development, audience];
+    var skillsDiv, skillsCols;
+    if (window.innerWidth < 440) {
+      skillsCols = skills.map((skill, i) => {
+        return (
+          <Col key={`skill${i + 1}`} xs={12 / skills.length}>
+            <div className="icon-container">
+              <Image src={`/assets/${skill.image}`} circle className={`rotate${skill.name == this.state.info.name ? ' selected' : ''}`} data-name={skill.name} onClick={this.handleClick} onMouseOver={this.rotate} onMouseOut={this.unrotate} />
+            </div>
+          </Col>
+        );
+      });
+      skillsDiv = (
+        <Row style={{ marginLeft: '-3.5rem' }}>
+          {skillsCols}
+        </Row>
       );
-    });
-    return (
-      <div className="container">
-        <Row>
-          <Col xs={10} xsOffset={1} md={4} mdOffset={4} className="text-center">
-            <h2>
-              What do I offer?
-            </h2>
-          </Col>
-        </Row>
+    } else {
+      skillsCols = skills.map((skill, i) => {
+        return (
+          <li key={`skill${i + 1}`}>
+            <div className="title-container">
+              <h3 id={`${skill.name.toLowerCase()}Title`} className={`title`}>{skill.name}</h3>
+            </div>
+            <div className="icon-container">
+              <Image src={`/assets/${skill.image}`} circle className={`rotate${skill.name == this.state.info.name ? ' selected' : ''}`} data-name={skill.name} onClick={this.handleClick} onMouseOver={this.rotate} onMouseOut={this.unrotate} />
+            </div>
+          </li>
+        );
+      });
+      skillsDiv = (
+        <ul className="text-right">
+          {skillsCols}
+        </ul>
+      );
+    }
 
-        <Row>
-          <Col sm={3} smOffset={0} md={4}>
-            <ul className="text-right">
-              {skillsDiv}
-            </ul>
-          </Col>
+    if (window.innerWidth < 440) {
+      return (
+        <div className="container">
+          <Row>
+            <Col xs={12} xsOffset={0} md={4} mdOffset={4} className="text-center">
+              <h2>
+                What do I offer?
+              </h2>
+            </Col>
+          </Row>
 
-          <Col className="skill-content" xs={10} xsOffset={1} sm={6} smOffset={3} md={6} mdOffset={2}>
-            <Row>
-              <Col xs={2} xsOffset={4}>
-                <div className="title-container">
-                  <h3 id={`insert-title`} className={`title ${this.state.info.name.toLowerCase()}`}>{window.innerWidth < 992 ? this.state.info.name : null}</h3>
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={12}>
-                <div className="content-container">
-                  <span>{this.state.info.content}</span>
-                </div>
-              </Col>
-            </Row>
+          {skillsDiv}
+
+          <Row>
+            <Col className="skill-content" xs={10} xsOffset={1} sm={6} smOffset={3} md={6} mdOffset={2}>
+              <Row>
+                <Col xs={2} xsOffset={4}>
+                  <div className="title-container">
+                    <h3 id={`insert-title`} className={`title ${this.state.info.name.toLowerCase()}`}>{window.innerWidth < 992 ? this.state.info.name : null}</h3>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12}>
+                  <div className="content-container">
+                    <span>{this.state.info.content}</span>
+                  </div>
+                </Col>
+              </Row>
 
 
-          </Col>
-        </Row>
-      </div>
-    );
+            </Col>
+          </Row>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <Row>
+            <Col xs={12} xsOffset={0} md={4} mdOffset={4} className="text-center">
+              <h2>
+                What do I offer?
+              </h2>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={0} sm={3} smOffset={0} md={4}>
+                {skillsDiv}
+            </Col>
+
+            <Col className="skill-content" xs={10} xsOffset={1} sm={6} smOffset={3} md={6} mdOffset={2}>
+              <Row>
+                <Col xs={2} xsOffset={4}>
+                  <div className="title-container">
+                    <h3 id={`insert-title`} className={`title ${this.state.info.name.toLowerCase()}`}>{window.innerWidth < 992 ? this.state.info.name : null}</h3>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12}>
+                  <div className="content-container">
+                    <span>{this.state.info.content}</span>
+                  </div>
+                </Col>
+              </Row>
+
+
+            </Col>
+          </Row>
+        </div>
+      );
+    }
   }
 }
