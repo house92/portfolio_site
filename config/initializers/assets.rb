1f15dc7c5d33828333fb932d8fc7cf05855ccef5
Sam <samuel.house@ntlworld.com>
2017-07-16 22:06:36 +0100

    moved project to new webpack format

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,15 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Dir[Rails.root.join('app/controllers/*_controller.rb')].map do |path|
-  path.match(/(\w+)_controller.rb/)
-  $1
-end.compact.each do |controller|
-  Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
-end
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
--- test/controllers/snippets_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SnippetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get snippets_index_url
-    assert_response :success
-  end
-
-end
