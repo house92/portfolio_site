4064517f001976be3caa961252a5a2d819c8ed00
Sam <samuel.house@ntlworld.com>
2017-07-17 01:56:12 +0100

    fingerprinted assets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all.order(id: :asc)
+    @projects = Project.all.order(id: :asc).as_json(methods: :fingerprinted_image)
   end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+
+  def fingerprinted_image
+    ActionController::Base.helpers.image_path(image)
+  end
 end
